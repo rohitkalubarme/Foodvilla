6d1be7ff7a363e76c3c9d72dd49531a4a025a3fb
Rohit kalubarme <rohitkalubarme18@gmail.com>
2020-11-29 12:11:50 +0530

    initial

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const expressLayout = require('express-ejs-layouts')
 
 const PORT =process.env.PORT || 3000
 
+app.use(express.static('public'))
 app.get('/', (req ,res) => {
     res.render('home')
 })
