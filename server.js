58c6e0c125ca99285e4c3845c20d279341e20de1
Rohit kalubarme <rohitkalubarme18@gmail.com>
2020-12-13 07:32:50 +0530

    initial

--- server.js
+++ server.js
@@ -1,35 +1,32 @@
 const express = require('express')
-const app = express()
+const app =  express()
 const ejs = require('ejs')
 const path = require('path')
 const expressLayout = require('express-ejs-layouts')
+const PORT =process.env.PORT || 3006
+// const mongoose = require('mongoose')
+// const url ="mongodb://localhost/pizza";
 
-const PORT =process.env.PORT || 3004
+// Database connection
+// mongoose.connect(process.env.MONGO_CONNECTION_URL, { useNewUrlParser: true, useCreateIndex:true, useUnifiedTopology: true, useFindAndModify : true });
+// const connection = mongoose.connection;
+// connection.once('open', () => {
+//     console.log('Database connected...');
+// }).catch(err => {
+//     console.log('Connection failed...')
+// });
 
+// Assets
 
-
+app.use(express.static('public'))
 
 // set template engine
-
 app.use(expressLayout)
 app.set('views', path.join(__dirname, '/resources/views'))
 app.set('view engine', 'ejs')
 
-app.use(express.static('public'))
-app.get('/', (req ,res) => {
-    res.render('home')
-})
+ require('./routes/web')(app)
 
-app.get('/cart',(req, res)=>{
-    res.render('customers/cart')
-})
-
-app.get('/login',(req, res)=>{
-    res.render('auth/login')
-})
-app.get('/register',(req, res)=>{
-    res.render('auth/register')
-})
 
 app.listen(PORT , () => {
     console.log(`listening on port ${PORT}`)
