6210df0ac8c34744ed51f8ccae2589ddf9a81f9a
Rohit kalubarme <rohitkalubarme18@gmail.com>
2020-12-16 18:21:27 +0530

    add to cart

--- app/http/controllers/homeController.js
+++ app/http/controllers/homeController.js
@@ -1,13 +1,10 @@
 const Menu = require('../../models/menu')
 function homeController() {
     return {
-          async index (req, res){
-          const pizzas=await Menu.find()
-          
-          return res.render('home',{pizzas:pizzas})
-
-           
-         
+        async index(req,res){
+        const pizzas = await Menu.find()
+        console.log(pizzas);
+        return res.render('home',{pizzas:pizzas}) 
         }
     }
 }
--- routes/web.js
+++ routes/web.js
@@ -4,14 +4,13 @@
 
  
 function initRoutes(app) {
-   
+
     app.get('/',homeController().index)
-   
-    app.get('/cart',cartController().index)
     
     app.get('/login',authController().login)
-
-    app.get('/register',authController().register)
-    
+    app.get('/register',authController().register) 
+    app.get('/cart',cartController().index)
+    app.post('/update-cart',cartController().update)
 }
+
 module.exports = initRoutes
